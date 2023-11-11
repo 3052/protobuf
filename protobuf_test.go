562d01cf84c8ae1fbd37fc7f3c616c10f8d67c66
Steven Penny <spen+3052@protonmail.com>
2023-11-11 00:16:42 -0600

    +         m.Add_Varint(10, 30)

--- protobuf_test.go
+++ protobuf_test.go
@@ -10,32 +10,31 @@ func message_new() []byte {
    var m Message
    m.Add(4, func(m *Message) {
       m.Add(1, func(m *Message) {
-         m.Add_Varint(10, 28) // int sdkVersion_
+         m.Add_Varint(10, 30)
       })
-      m.Add_Varint(18, 1)
    })
-   m.Add_Varint(14, 3) // int version
+   m.Add_Varint(14, 3)
    m.Add(18, func(m *Message) {
-      m.Add_Varint(1, 1) // int touchScreen
-      m.Add_Varint(2, 1) // int keyboard
-      m.Add_Varint(3, 1) // int navigation
-      m.Add_Varint(4, 1) // int screenLayout
+      m.Add_Varint(1, 3)
+      m.Add_Varint(2, 2)
+      m.Add_Varint(3, 2)
+      m.Add_Varint(4, 2)
       m.Add_Varint(5, 1)
       m.Add_Varint(6, 1)
-      m.Add_Varint(7, 1) // int screenDensity
-      m.Add_Varint(8, 1) // int glEsVersion
+      m.Add_Varint(7, 420)
+      m.Add_Varint(8, 0x30001)
       for _, lib := range libs {
-         m.Add_String(9, lib) // String[] systemSharedLibrary
+         m.Add_String(9, lib)
       }
-      m.Add_String(11, "hello") // String[] nativePlatform
+      m.Add_String(11, "hello")
       for _, ext := range exts {
-         m.Add_String(15, ext) // String[] glExtension
+         m.Add_String(15, ext)
       }
-      m.Add(26, func(m *Message) {
-         for _, feat := range feats {
-            m.Add_String(1, feat) // String[] systemAvailableFeature
-         }
-      })
+      for _, feat := range feats {
+         m.Add(26, func(m *Message) {
+            m.Add_String(1, feat)
+         })
+      }
    })
    return m.Append(nil)
 }
@@ -57,21 +56,20 @@ func message_old() []byte {
    return protopack.Message{
       protopack.Tag{4, protopack.BytesType}, protopack.LengthPrefix{
          protopack.Tag{1, protopack.BytesType}, protopack.LengthPrefix{
-            protopack.Tag{10, protopack.VarintType}, protopack.Varint(28),
+            protopack.Tag{10, protopack.VarintType}, protopack.Varint(30),
          },
-         protopack.Tag{18, protopack.VarintType}, protopack.Varint(1),
       },
       protopack.Tag{14, protopack.VarintType}, protopack.Varint(3),
       protopack.Tag{18, protopack.BytesType}, protopack.LengthPrefix(func() []protopack.Token {
          m := []protopack.Token{
-            protopack.Tag{1, protopack.VarintType}, protopack.Varint(1),
-            protopack.Tag{2, protopack.VarintType}, protopack.Varint(1),
-            protopack.Tag{3, protopack.VarintType}, protopack.Varint(1),
-            protopack.Tag{4, protopack.VarintType}, protopack.Varint(1),
-            protopack.Tag{5, protopack.VarintType}, protopack.Bool(true),
-            protopack.Tag{6, protopack.VarintType}, protopack.Bool(true),
-            protopack.Tag{7, protopack.VarintType}, protopack.Varint(1),
-            protopack.Tag{8, protopack.VarintType}, protopack.Varint(1),
+            protopack.Tag{1, protopack.VarintType}, protopack.Varint(3),
+            protopack.Tag{2, protopack.VarintType}, protopack.Varint(2),
+            protopack.Tag{3, protopack.VarintType}, protopack.Varint(2),
+            protopack.Tag{4, protopack.VarintType}, protopack.Varint(2),
+            protopack.Tag{5, protopack.VarintType}, protopack.Varint(1),
+            protopack.Tag{6, protopack.VarintType}, protopack.Varint(1),
+            protopack.Tag{7, protopack.VarintType}, protopack.Varint(420),
+            protopack.Tag{8, protopack.VarintType}, protopack.Varint(0x30001),
          }
          for _, lib := range libs {
             m = append(m,
@@ -86,17 +84,13 @@ func message_old() []byte {
                protopack.Tag{15, protopack.BytesType}, protopack.String(ext),
             )
          }
-         m = append(m,
-            protopack.Tag{26, protopack.BytesType}, protopack.LengthPrefix(func() []protopack.Token {
-               var m []protopack.Token
-               for _, feat := range feats {
-                  m = append(m,
-                     protopack.Tag{1, protopack.BytesType}, protopack.String(feat),
-                  )
-               }
-               return m
-            }()),
-         )
+         for _, feat := range feats {
+            m = append(m,
+               protopack.Tag{26, protopack.BytesType}, protopack.LengthPrefix{
+                  protopack.Tag{1, protopack.BytesType}, protopack.String(feat),
+               },
+            )
+         }
          return m
       }()),
    }.Marshal()
