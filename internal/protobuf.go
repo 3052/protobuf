1288adb63489cff666b377fae594ea63d2cf03b8
3052 <spen+3052@protonmail.com>
2024-01-28 18:28:05 -0600

    -   "io"

--- internal/protobuf.go
+++ internal/protobuf.go
@@ -2,6 +2,14 @@ package protobuf
 
 import "google.golang.org/protobuf/encoding/protowire"
 
+func add(m *Message, n Number, t Type, v Value) {
+   *m = append(*m, Field{
+      Number: n,
+      Type: t,
+      Value: v,
+   })
+}
+
 func get[T Value](m Message, n Number, f func(T) bool) {
    for _, record := range m {
       if record.Number == n {
@@ -36,6 +44,23 @@ type Fixed64 uint64
 
 func (f Fixed64) Append(b []byte) []byte {
    return protowire.AppendFixed64(b, uint64(f))
+}
+
+type Message []Field
+
+func (m Message) Append(b []byte) []byte {
+   return protowire.AppendBytes(b, m.Encode())
+}
+
+func (m Message) Encode() []byte {
+   var b []byte
+   for _, f := range m {
+      if f.Type >= 0 {
+         b = protowire.AppendTag(b, f.Number, f.Type)
+         b = f.Value.Append(b)
+      }
+   }
+   return b
 }
 
 type Number = protowire.Number
