c97adba3d93eea7aa2ab158003882fe738b788c8
3052 <spen+3052@protonmail.com>
2024-08-18 21:12:30 -0500

    +type Values interface {

--- values.go
+++ values.go
@@ -1,9 +1,5 @@
 package protobuf
 
-func add[T Value, U Values](m U, key Number, v T) {
-   m[key] = append(m[key], v)
-}
-
 func get[T Value, U Values](m U, key Number) chan T {
    channel := make(chan T)
    go func() {
@@ -19,49 +15,25 @@ func get[T Value, U Values](m U, key Number) chan T {
 }
 
 func (m Message) AddVarint(key Number, v Varint) {
-   add(m, key, v)
+   m[key] = append(m[key], v)
 }
 
 func (m Message) AddFixed64(key Number, v Fixed64) {
-   add(m, key, v)
+   m[key] = append(m[key], v)
 }
 
 func (m Message) AddFixed32(key Number, v Fixed32) {
-   add(m, key, v)
+   m[key] = append(m[key], v)
 }
 
 func (m Message) AddBytes(key Number, v Bytes) {
-   add(m, key, v)
+   m[key] = append(m[key], v)
 }
 
 func (m Message) Add(key Number, f func(Message)) {
    v := Message{}
    f(v)
-   add(m, key, v)
-}
-
-func (m Message) GetVarint(key Number) chan Varint {
-   return get[Varint](m, key)
-}
-
-func (m Message) GetFixed64(key Number) chan Fixed64 {
-   return get[Fixed64](m, key)
-}
-
-func (m Message) GetFixed32(key Number) chan Fixed32 {
-   return get[Fixed32](m, key)
-}
-
-func (m Message) GetBytes(key Number) chan Bytes {
-   return get[Bytes](m, key)
-}
-
-func (m Message) Get(key Number) chan Message {
-   return get[Message](m, key)
-}
-
-func (m Message) GetUnknown(key Number) chan UnknownMessage {
-   return get[UnknownMessage](m, key)
+   m[key] = append(m[key], v)
 }
 
 func (u UnknownMessage) GetVarint(key Number) chan Varint {
@@ -82,4 +54,24 @@ func (u UnknownMessage) GetBytes(key Number) chan Bytes {
 
 func (u UnknownMessage) Get(key Number) chan UnknownMessage {
    return get[UnknownMessage](u, key)
+}
+
+func (m Message) GetVarint(key Number) chan Varint {
+   return get[Varint](m, key)
+}
+
+func (m Message) GetFixed64(key Number) chan Fixed64 {
+   return get[Fixed64](m, key)
+}
+
+func (m Message) GetFixed32(key Number) chan Fixed32 {
+   return get[Fixed32](m, key)
+}
+
+func (m Message) GetBytes(key Number) chan Bytes {
+   return get[Bytes](m, key)
+}
+
+func (m Message) GetUnknown(key Number) chan UnknownMessage {
+   return get[UnknownMessage](m, key)
 }
