c2cf6dc417442cd9aaf406b44cf8cdfcabdddad3
3052 <spen+3052@protonmail.com>
2025-02-09 12:58:20 -0600

    +func (v Varint) GoString() string {

--- internal/protobuf/protobuf.go
+++ internal/protobuf/protobuf.go
@@ -7,34 +7,6 @@ import (
    "slices"
 )
 
-func consume_i64(data []byte) ([]I64, error) {
-   var vs []I64
-   for len(data) >= 1 {
-      v, n := protowire.ConsumeFixed64(data)
-      err := protowire.ParseError(n)
-      if err != nil {
-         return nil, err
-      }
-      vs = append(vs, I64(v))
-      data = data[n:]
-   }
-   return vs, nil
-}
-
-func consume_i32(data []byte) ([]I32, error) {
-   var vs []I32
-   for len(data) >= 1 {
-      v, n := protowire.ConsumeFixed32(data)
-      err := protowire.ParseError(n)
-      if err != nil {
-         return nil, err
-      }
-      vs = append(vs, I32(v))
-      data = data[n:]
-   }
-   return vs, nil
-}
-
 type Bytes []byte
 
 func (b Bytes) Append(data []byte) []byte {
@@ -72,54 +44,6 @@ func (i I64) GoString() string {
 
 func (n *Len) Append(data []byte) []byte {
    return protowire.AppendBytes(data, n.Bytes)
-}
-
-type Len struct {
-   Bytes   Bytes
-   I32     []I32
-   I64     []I64
-   Message Message
-   Varint  []Varint
-}
-
-func (n *Len) GoString() string {
-   b := []byte("&protobuf.Len{\n")
-   b = fmt.Appendf(b, "Bytes:%#v,\n", n.Bytes)
-   if n.Varint != nil {
-      b = append(b, "Varint:[]protobuf.Varint{"...)
-      for key, value0 := range n.Varint {
-         if key >= 1 {
-            b = append(b, ',')
-         }
-         b = fmt.Append(b, value0)
-      }
-      b = append(b, "},\n"...)
-   }
-   if n.I32 != nil {
-      b = append(b, "I32:[]protobuf.I32{"...)
-      for key, value0 := range n.I32 {
-         if key >= 1 {
-            b = append(b, ',')
-         }
-         b = fmt.Append(b, value0)
-      }
-      b = append(b, "},\n"...)
-   }
-   if n.I64 != nil {
-      b = append(b, "I64:[]protobuf.I64{"...)
-      for key, value0 := range n.I64 {
-         if key >= 1 {
-            b = append(b, ',')
-         }
-         b = fmt.Append(b, value0)
-      }
-      b = append(b, "},\n"...)
-   }
-   if n.Message != nil {
-      b = fmt.Appendf(b, "Message:%#v,\n", n.Message)
-   }
-   b = append(b, '}')
-   return string(b)
 }
 
 func (m Message) Append(data []byte) []byte {
@@ -203,6 +127,21 @@ func (m *Message) unmarshal(data []byte) error {
    return nil
 }
 
+func (v Varint) GoString() string {
+   return fmt.Sprintf("protobuf.Varint(%v)", v)
+}
+
+type Varint uint64
+
+func (v Varint) Append(data []byte) []byte {
+   return protowire.AppendVarint(data, uint64(v))
+}
+
+type value interface {
+   Append([]byte) []byte
+   fmt.GoStringer
+}
+
 func (m Message) GoString() string {
    data := []byte("protobuf.Message{\n")
    for _, f := range m {
@@ -212,67 +151,29 @@ func (m Message) GoString() string {
    return string(data)
 }
 
-func (v Varint) GoString() string {
-   return fmt.Sprintf("protobuf.Varint(%v)", v)
+// this can also be package repeated fields:
+// protobuf.dev/programming-guides/encoding#structure
+// but in practice I have never come across those, even with JSON
+type Len struct {
+   Bytes   Bytes
+   Message Message
 }
 
-type value interface {
-   Append([]byte) []byte
-   fmt.GoStringer
+func (n *Len) GoString() string {
+   b := []byte("&protobuf.Len{\n")
+   b = fmt.Appendf(b, "%#v,\n", n.Bytes)
+   b = fmt.Appendf(b, "%#v,\n", n.Message)
+   b = append(b, '}')
+   return string(b)
 }
 
 func unmarshal(data []byte) value {
    data = slices.Clip(data)
    if len(data) >= 1 {
-      var len0 *Len
-      if v, err := consume_i32(data); err == nil {
-         len0 = &Len{I32: v}
-      }
-      if v, err := consume_i64(data); err == nil {
-         if len0 == nil {
-            len0 = &Len{}
-         }
-         len0.I64 = v
-      }
-      var v Message
-      if v.unmarshal(data) == nil {
-         if len0 == nil {
-            len0 = &Len{}
-         }
-         len0.Message = v
-      }
-      if v, err := consume_varint(data); err == nil {
-         if len0 == nil {
-            len0 = &Len{}
-         }
-         len0.Varint = v
-      }
-      if len0 != nil {
-         len0.Bytes = data
-         return len0
+      var m Message
+      if m.unmarshal(data) == nil {
+         return &Len{data, m}
       }
    }
    return Bytes(data)
-}
-
-///
-
-func consume_varint(data []byte) ([]Varint, error) {
-   var vs []Varint
-   for len(data) >= 1 {
-      v, n := protowire.ConsumeVarint(data)
-      err := protowire.ParseError(n)
-      if err != nil {
-         return nil, err
-      }
-      vs = append(vs, Varint(v))
-      data = data[n:]
-   }
-   return vs, nil
-}
-
-type Varint uint64
-
-func (v Varint) Append(data []byte) []byte {
-   return protowire.AppendVarint(data, uint64(v))
 }
