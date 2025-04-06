fa97724b48d938673c41bc523828fa923265e10f
367 <367@tuta.io>
2025-04-06 18:14:44 -0500

    deleted:    json_test.go

--- protobuf.go
+++ protobuf.go
@@ -7,111 +7,6 @@ import (
    "iter"
 )
 
-type Number = protowire.Number
-
-func (v Varint) GoString() string {
-   return fmt.Sprintf("protobuf.Varint(%v)", v)
-}
-
-type Varint uint64
-
-type Bytes []byte
-
-type I32 uint32
-
-type I64 uint64
-
-type Message []Record
-
-func (b Bytes) GoString() string {
-   return fmt.Sprintf("protobuf.Bytes(%q)", []byte(b))
-}
-
-func (b Bytes) MarshalText() ([]byte, error) {
-   return b, nil
-}
-
-func (i I32) MarshalText() ([]byte, error) {
-   return fmt.Appendf(nil, "I32(%v)", i), nil
-}
-
-func (i I64) MarshalText() ([]byte, error) {
-   return fmt.Appendf(nil, "I64(%v)", i), nil
-}
-
-func (v Varint) MarshalText() ([]byte, error) {
-   return fmt.Appendf(nil, "Varint(%v)", v), nil
-}
-
-// protobuf.dev/programming-guides/encoding#structure
-type Record struct {
-   Number  Number
-   Payload Payload
-}
-
-func (i I32) GoString() string {
-   return fmt.Sprintf("protobuf.I32(%v)", i)
-}
-
-func (i I64) GoString() string {
-   return fmt.Sprintf("protobuf.I64(%v)", i)
-}
-
-type LenPrefix struct {
-   Bytes   Bytes
-   Message Message
-}
-
-func (p *LenPrefix) GoString() string {
-   data := []byte("&protobuf.LenPrefix{\n")
-   data = fmt.Appendf(data, "%#v,\n", p.Bytes)
-   data = fmt.Appendf(data, "%#v,\n", p.Message)
-   data = append(data, '}')
-   return string(data)
-}
-
-type Payload interface {
-   Append([]byte, Number) []byte
-}
-
-func (b Bytes) Append(data []byte, num Number) []byte {
-   data = protowire.AppendTag(data, num, protowire.BytesType)
-   return protowire.AppendBytes(data, b)
-}
-
-func (m Message) GetVarint(num Number) iter.Seq[Varint] {
-   return get[Varint](m, num)
-}
-
-func (m Message) GetI64(num Number) iter.Seq[I64] {
-   return get[I64](m, num)
-}
-
-func (m *Message) AddVarint(num Number, value Varint) {
-   *m = append(*m, Record{num, value})
-}
-
-func (m *Message) AddI64(num Number, value I64) {
-   *m = append(*m, Record{num, value})
-}
-
-func (i I32) Append(data []byte, num Number) []byte {
-   data = protowire.AppendTag(data, num, protowire.Fixed32Type)
-   return protowire.AppendFixed32(data, uint32(i))
-}
-
-func (m *Message) AddI32(num Number, value I32) {
-   *m = append(*m, Record{num, value})
-}
-
-func (m *Message) AddBytes(num Number, value Bytes) {
-   *m = append(*m, Record{num, value})
-}
-
-func (m Message) GetI32(num Number) iter.Seq[I32] {
-   return get[I32](m, num)
-}
-
 func get[P Payload](m Message, num Number) iter.Seq[P] {
    return func(yield func(P) bool) {
       for _, record1 := range m {
@@ -127,9 +22,49 @@ func get[P Payload](m Message, num Number) iter.Seq[P] {
    }
 }
 
+func (b Bytes) Append(data []byte, num Number) []byte {
+   data = protowire.AppendTag(data, num, protowire.BytesType)
+   return protowire.AppendBytes(data, b)
+}
+
+type Bytes []byte
+
+func (b Bytes) GoString() string {
+   return fmt.Sprintf("protobuf.Bytes(%q)", []byte(b))
+}
+
+func (b Bytes) MarshalText() ([]byte, error) {
+   return b, nil
+}
+
+func (i I32) Append(data []byte, num Number) []byte {
+   data = protowire.AppendTag(data, num, protowire.Fixed32Type)
+   return protowire.AppendFixed32(data, uint32(i))
+}
+
+type I32 uint32
+
+func (i I32) MarshalText() ([]byte, error) {
+   return fmt.Appendf(nil, "I32(%v)", i), nil
+}
+
+func (i I32) GoString() string {
+   return fmt.Sprintf("protobuf.I32(%v)", i)
+}
+
 func (i I64) Append(data []byte, num Number) []byte {
    data = protowire.AppendTag(data, num, protowire.Fixed64Type)
    return protowire.AppendFixed64(data, uint64(i))
+}
+
+type I64 uint64
+
+func (i I64) MarshalText() ([]byte, error) {
+   return fmt.Appendf(nil, "I64(%v)", i), nil
+}
+
+func (i I64) GoString() string {
+   return fmt.Sprintf("protobuf.I64(%v)", i)
 }
 
 func (p *LenPrefix) Append(data []byte, num Number) []byte {
@@ -137,9 +72,17 @@ func (p *LenPrefix) Append(data []byte, num Number) []byte {
    return protowire.AppendBytes(data, p.Bytes)
 }
 
-func (v Varint) Append(data []byte, num Number) []byte {
-   data = protowire.AppendTag(data, num, protowire.VarintType)
-   return protowire.AppendVarint(data, uint64(v))
+type LenPrefix struct {
+   Bytes   Bytes
+   Message Message
+}
+
+func (p *LenPrefix) GoString() string {
+   data := []byte("&protobuf.LenPrefix{\n")
+   data = fmt.Appendf(data, "%#v,\n", p.Bytes)
+   data = fmt.Appendf(data, "%#v,\n", p.Message)
+   data = append(data, '}')
+   return string(data)
 }
 
 func (m Message) Marshal() []byte {
@@ -172,16 +115,6 @@ func (m *Message) Add(num Number, value func(*Message)) {
    var m1 Message
    value(&m1)
    *m = append(*m, Record{num, m1})
-}
-
-func unmarshal(data []byte) Payload {
-   if len(data) >= 1 {
-      var m Message
-      if m.Unmarshal(data) == nil {
-         return &LenPrefix{data, m}
-      }
-   }
-   return Bytes(data)
 }
 
 func (m Message) Get(num Number) iter.Seq[Message] {
@@ -278,4 +211,72 @@ func (m *Message) Unmarshal(data []byte) error {
       *m = append(*m, r)
    }
    return nil
+}
+
+type Message []Record
+
+func (m Message) GetVarint(num Number) iter.Seq[Varint] {
+   return get[Varint](m, num)
+}
+
+func (m Message) GetI64(num Number) iter.Seq[I64] {
+   return get[I64](m, num)
+}
+
+func (m *Message) AddVarint(num Number, value Varint) {
+   *m = append(*m, Record{num, value})
+}
+
+func (m *Message) AddI64(num Number, value I64) {
+   *m = append(*m, Record{num, value})
+}
+
+func (m *Message) AddI32(num Number, value I32) {
+   *m = append(*m, Record{num, value})
+}
+
+func (m *Message) AddBytes(num Number, value Bytes) {
+   *m = append(*m, Record{num, value})
+}
+
+func (m Message) GetI32(num Number) iter.Seq[I32] {
+   return get[I32](m, num)
+}
+
+type Number = protowire.Number
+
+func unmarshal(data []byte) Payload {
+   if len(data) >= 1 {
+      var m Message
+      if m.Unmarshal(data) == nil {
+         return &LenPrefix{data, m}
+      }
+   }
+   return Bytes(data)
+}
+
+type Payload interface {
+   Append([]byte, Number) []byte
+   fmt.GoStringer
+}
+
+// protobuf.dev/programming-guides/encoding#structure
+type Record struct {
+   Number  Number
+   Payload Payload
+}
+
+func (v Varint) GoString() string {
+   return fmt.Sprintf("protobuf.Varint(%v)", v)
+}
+
+type Varint uint64
+
+func (v Varint) MarshalText() ([]byte, error) {
+   return fmt.Appendf(nil, "Varint(%v)", v), nil
+}
+
+func (v Varint) Append(data []byte, num Number) []byte {
+   data = protowire.AppendTag(data, num, protowire.VarintType)
+   return protowire.AppendVarint(data, uint64(v))
 }
