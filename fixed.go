627236b104eedaf029b37c1a04d606b9d9cab2e3
27 <27@riseup.net>
2025-11-21 06:34:18 -0600

    renamed:    fixed32.go -> fixed.go

--- fixed.go
+++ fixed.go
@@ -5,6 +5,14 @@ import (
    "errors"
 )
 
+// ParseFixed32 parses a 32-bit little-endian integer from the buffer.
+func ParseFixed32(buf []byte) (uint32, int, error) {
+   if len(buf) < 4 {
+      return 0, 0, errors.New("buffer is too small for a fixed32")
+   }
+   return binary.LittleEndian.Uint32(buf), 4, nil
+}
+
 // ParseFixed64 parses a 64-bit little-endian integer from the buffer.
 func ParseFixed64(buf []byte) (uint64, int, error) {
    if len(buf) < 8 {
