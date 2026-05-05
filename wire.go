f73917f544c6460b4509c1a5e248dc15067399d0
27 <27@riseup.net>
2026-05-05 17:16:07 -0500

    +   ErrOutOfBounds = errors.New("data is out of bounds")

--- wire.go
+++ wire.go
@@ -1,7 +1,24 @@
 // wire.go
 package protobuf
 
-import "encoding/binary"
+import (
+   "encoding/binary"
+   "errors"
+)
+
+var (
+   // ErrOutOfBounds is returned when attempting to read past the end of the buffer.
+   ErrOutOfBounds = errors.New("data is out of bounds")
+
+   // ErrMalformedVarint is returned when a varint is unterminated or overflows.
+   ErrMalformedVarint = errors.New("malformed varint")
+
+   // ErrBufferTooSmall is returned when a buffer does not contain enough bytes for a fixed-size value.
+   ErrBufferTooSmall = errors.New("buffer is too small")
+
+   // ErrInvalidWireType is returned when an unknown or unsupported wire type is encountered.
+   ErrInvalidWireType = errors.New("invalid wire type")
+)
 
 // DecodeVarint reads a varint from the buffer and returns the decoded uint64 and the number of bytes read.
 // A negative number of bytes indicates an overflow. A zero indicates an unterminated varint.
